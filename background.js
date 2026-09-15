68379299e4522aa2666cd0bb8067954edaa25163
werriesjacob1-cmyk <werriesjacob1@gmail.com>
2026-09-15 09:07:58 -0500

    Raise overnight session claim cap to 200

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ const DEFAULTS = Object.freeze({
   debug: false,
   maxLeadAgeMin: 5,
   minClaimIntervalSec: 10,
-  maxClaimsPerSession: 10,
+  maxClaimsPerSession: 200,
   returnToList: true,
   returnDelaySec: 5,
   myName: ''
